bf966788d589ea15528a589be1a9d245451cb17d
sulthann1 <sulthanaawaliya155@gmail.com>
2026-08-25 21:26:05 +0700

    fixing terus

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 import "dotenv/config";
-
 import express from "express";
-
-import authRoutes
-  from "./routes/authRoutes.js";
-
-import apiKeyRoutes
-  from "./routes/apiKeyRoutes.js";
-
-import cafeRoutes
-  from "./routes/cafeRoutes.js";
-
+import authRoutes from "./routes/authRoutes.js";
+import apiKeyRoutes from "./routes/apiKeyRoutes.js";
+import cafeRoutes from "./routes/cafeRoutes.js";
 
 const app = express();
 
 app.use(express.json());
-
 
 app.get("/", (req, res) => {
   res.json({
@@ -24,22 +15,9 @@ app.get("/", (req, res) => {
   });
 });
 
-
-app.use(
-  "/api/auth",
-  authRoutes
-);
-
-app.use(
-  "/api/keys",
-  apiKeyRoutes
-);
-
-app.use(
-  "/api/v1/cafes",
-  cafeRoutes
-);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/keys", apiKeyRoutes);
+app.use("/api/v1/cafes", cafeRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
@@ -48,14 +26,13 @@ app.use((req, res) => {
   });
 });
 
-
-const PORT =
-  process.env.PORT || 3000;
-
-
+// Server hanya 'listen' secara lokal
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
+
+// BARIS INI WAJIB ADA UNTUK VERCEL
+export default app;
